208d51c812f779a868020491a5a25ca2aca7b732
6ui4r4uj0 <62915262+GuiAralopes@users.noreply.github.com>
2022-06-07 18:10:52 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,21 @@
 let nome = document.getElementById("nome");
 let email = document.getElementById("email");
+let password = document.getElementById("password");
 let form = document.querySelector("form");
 let textForm = document.getElementById("textForm");
 let textEmail = document.getElementById("textEmail");
 
 form.addEventListener("submit", (e) => {
-  if (nome.value == "" || email.value == "" || senha.value == "") {
+  if (nome.value == "" || email.value == "" || password.value == "") {
     textForm.textContent = "Preencha todos os Campos!";
   } else {
     textForm.textContent = "Mensagem enviada com Sucesso!";
     console.log(nome.value);
     console.log(email.value);
-    console.log(senha.value);
+    console.log(password.value);
     nome.value = "";
     email.value = "";
-    senha.value = "";
+    password.value = "";
   }
   e.preventDefault();
 });
